4287ccce8e2e0616db75f8a72ce41e8e2e59cd32
Brian <102098077+BrianWBurke@users.noreply.github.com>
2022-08-31 14:02:56 -0300

    ex 2.1

---
+++ Condicional/exercício2.1.py
@@ -0,0 +1,21 @@
+print('Olá, seja bem vindo ao hortifruti! Escolha o que deseja comprar: ')
+print('Por favor digite o número correspondente do seu pedido:')
+print('1-Maça (R$2,30)')
+print('2-Laranja (R$3,60)')
+print('3-Banana (R$1,85)')
+fruta = int(input('Sua escolha é: '))
+
+if (fruta < 1 or fruta > 3):
+    print('Produto inexistente!')
+
+else:    
+    qnt = int(input('Por favor, digite a quantidade que desejada: '))
+if (fruta == 1):
+    total = qnt * 2.3
+    print(f'Você comprou {qnt} maça(s). Total à pagar: R$ {total:.2f}')
+elif (fruta == 2):
+    total = qnt * 3.6
+    print(f'Você comprou {qnt} laranja(s). Total à pagar: R$ {total:.2f}')
+elif (fruta == 3):
+    total = qnt * 1.85
+    print(f'Você comprou {qnt} banana(s). Total à pagar: R$ {total:.2f}')
