4f5f9031e45215f14c547700dbf90abc51eb0973
Brian <102098077+BrianWBurke@users.noreply.github.com>
2022-08-31 12:34:02 -0300

    exercicios 1 e 2 aula 3

---
+++ Condicional/condicional combosta.py
@@ -0,0 +1,7 @@
+# Desenvolva um programa que lê um valor inteiro e descobre se ele é par ou impar
+
+x = int(input('Digite um valor inteiro: '))
+if (x % 2 == 0):
+  print('O número é par!')
+else:
+  print('O número é impar!')
